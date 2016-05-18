ac50eb88f775b7c5384f5b8414f093220d371dee
Christopher A. DeFlumeri <deflume1@gmail.com>
2016-05-18 20:02:46 +0100

    Adding semantic versioning

--- Source/Zencoder/Properties/AssemblyInfo.cs
+++ Source/Zencoder/Properties/AssemblyInfo.cs
@@ -13,3 +13,21 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription(".NET client library for the Zencoder API.")]
 [assembly: Guid("e7c51f55-31dd-4a4c-9842-9b4c2aeef669")]
 [assembly: CLSCompliant(true)]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: InternalsVisibleTo("Zencoder.Test")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+// Used for NuGet packaging, uses semantic versioning: major.minor.patch-prerelease.
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+// Keep this the same as AssemblyInformationalVersion.
+[assembly: AssemblyFileVersion("1.0.0")]
+
+// ONLY change this when the major version changes; never with minor/patch/build versions.
+[assembly: AssemblyVersion("1.0.0.0")]
